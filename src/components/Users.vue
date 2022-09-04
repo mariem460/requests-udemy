0a3206ee9c7c11d2cad27b84af8dc65c9d62c2cc
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-09-04 22:01:39 +0200

    practise of requests

---
+++ src/components/Users.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="row">
+
+        <div 
+            class="col-auto mb-4"
+            v-for="(user) in users"
+            :key="user.id"
+        >
+            <div class="card" style="width: 14rem;">
+                <img 
+                    class="card-img-top" 
+                    :src="'https://placebeard.it/300/300?'+ user.id"  
+                    alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-text">
+                        <strong>Name:</strong> {{ user.name }}
+                    </div>
+                    <div class="card-text">
+                        <strong>Lastname:</strong> {{ user.lastname }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data(){
+            return {
+                users:[]
+            }
+        },
+        methods:{
+            loadUsers(){
+                axios.get('http://localhost:3000/users')
+                .then( response => {
+                    this.users = response.data
+                })
+                .catch( error => {
+                    console.log( error )
+                })
+            }  
+        },
+        mounted(){
+          this.loadUsers()
+        }
+    }
+
+</script>
